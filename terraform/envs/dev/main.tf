eb0a3d43fb4a46df6674cfab7257634eb33834e9
vkharishb <v.harish.bodapati@gmail.com>
2026-04-28 15:02:14 +0530

    k8s cluster version upgraded

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -4,12 +4,12 @@ module "vpc" {
   name = "eks-dev"
   cidr = "10.0.0.0/16"
 
-  azs = ["ap-south-1a", "ap-south-1b"]
+  azs = ["ap-south-2a", "ap-south-2b"]
 
   private_subnets = ["10.0.10.0/24", "10.0.20.0/24"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
-  single_nat_gateway = true
+  enable_nat_gateway = true
 
   tags = {
     env = "dev"
@@ -20,7 +20,7 @@ module "eks" {
   source = "../../modules/eks"
 
   cluster_name    = "eks-dev"
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
